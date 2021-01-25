5a728c5ca8ef042886c320d5ca693bb4cece2f0c
Eric Bram <61522167+sei-ebram@users.noreply.github.com>
2021-01-25 15:40:33 -0500

    remove unused httpcontext

--- Alloy.Api/Startup.cs
+++ Alloy.Api/Startup.cs
@@ -212,9 +212,6 @@ namespace Alloy.Api
                 c.OAuthClientSecret(_authOptions.ClientSecret);
                 c.OAuthAppName(_authOptions.ClientName);
             });
-
-
-            app.UseHttpContext();
         }
 
 
